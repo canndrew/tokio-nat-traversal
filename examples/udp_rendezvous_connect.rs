27974b2cf996f08b35f4d070ceb427580853b3a3
Andrew Cann <shum@canndrew.org>
2017-11-09 14:25:20 +0800

    Add udp rendezvous connect example

--- examples/udp_rendezvous_connect.rs
+++ examples/udp_rendezvous_connect.rs
@@ -1,5 +1,5 @@
 /// To use this, use a server with publicly accessible ports to act as a relay server between two
-/// connecting tcp streams. This relay server will act as the channel when negotiating the
+/// connecting udp sockets. This relay server will act as the channel when negotiating the
 /// rendezvous connect.
 ///
 /// For example, log into a VPS and run:
@@ -7,10 +7,10 @@
 ///
 /// The run this example on two machines, on seperate networks, both hidden behind NATs:
 ///
-/// $ cargo run --example tcp_rendezvous_connect -- <address of your vps>:45666 blah blah blah
-/// $ cargo run --example tcp_rendezvous_connect -- <address of your vps>:45667 blah blah blah
+/// $ cargo run --example udp_rendezvous_connect -- <address of your vps>:45666 blah blah blah
+/// $ cargo run --example udp_rendezvous_connect -- <address of your vps>:45667 blah blah blah
 /// 
-/// If successful, the peers should be able to form a TCP connection directly to each other.
+/// If successful, the peers should be able to communicate directly with each other over UDP.
 #[macro_use]
 extern crate unwrap;
 extern crate tokio_core;
@@ -22,12 +22,13 @@ extern crate bytes;
 extern crate void;
 
 use std::{env, fmt};
-use std::net::{SocketAddr, Shutdown};
+use std::net::SocketAddr;
 use std::str::FromStr;
-use tokio_core::net::TcpStream;
-use futures::{Future, Stream, Sink, Async, AsyncSink};
+use tokio_core::net::{TcpStream, UdpSocket};
+use futures::{future, Future, Stream, Sink, Async, AsyncSink};
+use futures::future::Loop;
 use tokio_io::codec::length_delimited::Framed;
-use tokio_nat_traversal::TcpStreamExt;
+use tokio_nat_traversal::UdpSocketExt;
 use void::ResultVoidExt;
 
 // TODO: figure out how to not need this.
@@ -68,7 +69,7 @@ fn main() {
         Some(relay_addr_str) => relay_addr_str,
         None => {
             println!("missing relay server address!");
-            println!("Usage: tcp_rendezvous_connect <relay server addr> <message to send>");
+            println!("Usage: udp_rendezvous_connect <relay server addr> <message to send>");
             return;
         },
     };
@@ -97,19 +98,26 @@ fn main() {
         .map_err(|e| panic!("error connecting to relay server: {}", e))
         .and_then(move |relay_stream| {
             let relay_channel = DummyDebug(Framed::new(relay_stream).map(|bytes| bytes.freeze()));
-            TcpStream::rendezvous_connect(relay_channel, &handle)
+            UdpSocket::rendezvous_connect(relay_channel, &handle)
             .map_err(|e| panic!("rendezvous connect failed: {}", e))
-            .and_then(|stream| {
+            .and_then(|(socket, addr)| {
                 println!("connected!");
-                tokio_io::io::write_all(stream, message)
-                .map_err(|e| panic!("error writing to tcp stream: {}", e))
-                .and_then(|(stream, _)| {
-                    unwrap!(stream.shutdown(Shutdown::Write));
-                    tokio_io::io::read_to_end(stream, Vec::new())
-                    .map_err(|e| panic!("error reading from tcp stream: {}", e))
-                    .map(|(_, data)| {
-                        let recv_message = String::from_utf8_lossy(&data);
-                        println!("got message: {}", recv_message);
+                socket
+                .send_dgram(message, addr)
+                .map_err(|e| panic!("error writing to udp socket: {}", e))
+                .and_then(move |(socket, _)| {
+                    future::loop_fn(socket, move |socket| {
+                        let buffer = vec![0u8; 64 * 1024];
+                        socket
+                        .recv_dgram(buffer)
+                        .map_err(|e| panic!("error receiving on udp socket: {}", e))
+                        .map(move |(socket, buffer, len, recv_addr)| {
+                            if recv_addr == addr {
+                                let recv_message = String::from_utf8_lossy(&buffer[..len]);
+                                println!("got message: {}", recv_message);
+                            }
+                            Loop::Continue(socket)
+                        })
                     })
                 })
             })
@@ -117,4 +125,5 @@ fn main() {
     });
     res.void_unwrap()
 }
+
 
